2098c2d43e610c80a5ebb9fc836c0aa558c2da7f
Dar Malovani <dar.malovani@wilcohq.com>
2022-05-15 14:04:57 +0300

    fix: make user items api endpoint faster

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -73,23 +73,17 @@ router.get("/", auth.optional, function(req, res, next) {
 
       return Promise.all([
         Item.find(query)
+          .populate("seller")
           .limit(Number(limit))
           .skip(Number(offset))
           .sort({ createdAt: "desc" })
           .exec(),
         Item.count(query).exec(),
-        req.payload ? User.findById(req.payload.id) : null
       ]).then(async function(results) {
         var items = results[0];
         var itemsCount = results[1];
-        var user = results[2];
         return res.json({
-          items: await Promise.all(
-            items.map(async function(item) {
-              item.seller = await User.findById(item.seller);
-              return item.toJSONFor(user);
-            })
-          ),
+          items,
           itemsCount: itemsCount
         });
       });
